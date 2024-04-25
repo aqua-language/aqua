d46aefd60010e962fd6b13761fe82fe0a896ce7d
Klas Segeljakt <klasseg@kth.se>
2024-04-25 15:43:16 +0200

    Update

--- crates/compiler/fuzz/fuzz_targets/fuzz_parser.rs
+++ crates/compiler/fuzz/fuzz_targets/fuzz_parser.rs
@@ -1,28 +1,8 @@
 #![no_main]
 
-use compiler::ast::Expr;
-use compiler::ast::Pat;
 use compiler::ast::Program;
-use compiler::ast::Stmt;
-use compiler::ast::Type;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     std::str::from_utf8(data).ok().map(Program::parse);
-});
-
-fuzz_target!(|data: &[u8]| {
-    std::str::from_utf8(data).ok().map(Expr::parse);
-});
-
-fuzz_target!(|data: &[u8]| {
-    std::str::from_utf8(data).ok().map(Type::parse);
-});
-
-fuzz_target!(|data: &[u8]| {
-    std::str::from_utf8(data).ok().map(Stmt::parse);
-});
-
-fuzz_target!(|data: &[u8]| {
-    std::str::from_utf8(data).ok().map(Pat::parse);
 });
